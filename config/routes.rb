eb6707b7380232db585c4a0d6650146b0268c22c
thecodelogix <thecodelogix@gmail.com>
2023-03-30 02:18:04 -0400

    Add room number and join link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#new"
   get "rooms/:number", to: "rooms#show", as: "room"
+  get "rooms/:number/join", to: "join#new", as: "join_room"
   resources :rooms, only: %i[show create update destroy] do
   end
 end
